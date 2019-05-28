3603c83c188c4bbc609304ec1c2246fa23f1fa40
Dustin <clerin+gitkraken@gmail.com>
2019-05-28 16:01:44 -0700

    starting stretch

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -5,30 +5,43 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
 
-const App = () => {
-  return (
-    <div className="calculator-app">
-      <CalculatorDisplay />
-      <div class="button-area">
-        <ActionButton text="clear"/>
-        <NumberButton text="÷" buttonStyle="operator" />
-        <NumberButton text="7" />
-        <NumberButton text="8" />
-        <NumberButton text="9" />
-        <NumberButton text="×" buttonStyle="operator" />
-        <NumberButton text="4" />
-        <NumberButton text="5" />
-        <NumberButton text="6" />
-        <NumberButton text="-" buttonStyle="operator" />
-        <NumberButton text="1" />
-        <NumberButton text="2" />
-        <NumberButton text="3" />
-        <NumberButton text="+" buttonStyle="operator" />
-        <ActionButton text="0" />
-        <NumberButton text="=" buttonStyle="operator" />
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      displayNumber: 0
+    }
+  }
+
+  render() {
+    return (
+      <div className="calculator-app">
+        <CalculatorDisplay displayValue={this.state.displayNumber} />
+        <div className="button-area">
+          <ActionButton function={this.clear} text="clear"/>
+          <NumberButton text="÷" buttonStyle="operator" />
+          <NumberButton text="7" />
+          <NumberButton text="8" />
+          <NumberButton text="9" />
+          <NumberButton text="×" buttonStyle="operator" />
+          <NumberButton text="4" />
+          <NumberButton text="5" />
+          <NumberButton text="6" />
+          <NumberButton text="-" buttonStyle="operator" />
+          <NumberButton text="1" />
+          <NumberButton text="2" />
+          <NumberButton text="3" />
+          <NumberButton text="+" buttonStyle="operator" />
+          <ActionButton text="0" />
+          <NumberButton text="=" buttonStyle="operator" />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+
+  clear() {
+    this.state.displayNumber = 0;
+  }
+}
 
 export default App;
